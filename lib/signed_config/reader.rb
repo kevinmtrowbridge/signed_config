4b455f6e2fbd45306c8d64845d48a285ee3883da
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-12-17 12:58:52 -0800

    Adding ruby method.

--- lib/signed_config/reader.rb
+++ lib/signed_config/reader.rb
@@ -1,10 +1,13 @@
 module SignedConfig
   class Reader
+    
     def initialize(public_key, signed_config)
       yml = signed_config.split("\n")[1..-2].join("\n")
       hash = YAML.load(yml)
       signature = hash[:signature]
       @config_string = hash[:config]
+
+      return if ruby_method
 
       unless Crypto.verify(public_key, signature, @config_string)
         raise SignatureDoesNotMatch
@@ -13,6 +16,10 @@ module SignedConfig
 
     def config
       YAML.load(@config_string)
+    end
+
+    def ruby_method
+      false
     end
   end
 end
